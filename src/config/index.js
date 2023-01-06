34feebed6b3dc4656b60a3bcf2d1d16adeae8a40
luanzixuan <yimuzx@foxmail.com>
2023-01-06 11:16:46 +0800

    关闭i18

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2020-05-11 10:57:35
- * @LastEditTime: 2022-03-01 09:55:22
- * @LastEditors: 赵婷婷
+ * @LastEditTime: 2023-01-06 11:14:56
+ * @LastEditors: 易木
  * @Description: In User Settings Edit
- * @FilePath: \files\src\config\index.js
+ * @FilePath: \sucai-modal-next\src\config\index.js
  */
 export default {
   /**
    * @description 配置显示在浏览器标签的title
    */
-  title: '素材弹窗',
+  title: "素材弹窗",
   /**
    * @description token在Cookie中存储的天数，默认1天
    */
@@ -21,27 +21,27 @@ export default {
    *              用来在菜单中显示文字
    */
   PAGE_SIZE: 10,
-  useI18n: true,
+  useI18n: false,
   /**
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: '',
-    pro: '',
+    dev: "",
+    pro: "",
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
    */
-  homeName: 'home',
+  homeName: "home",
   /**
    * @description 需要加载的插件
    */
   plugin: {
-    'error-store': {
+    "error-store": {
       showInHeader: false, // 设为false后不会在顶部显示错误日志徽标
       developmentOff: true, // 设为true后在开发环境不会收集错误信息，方便开发中排查错误
     },
   },
-  goBackUrl: 'http://backpack.shandian.design',
+  goBackUrl: "http://backpack.shandian.design",
   // goBackUrl: 'http://user.shandian.design'
 };
